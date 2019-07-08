eff2781e6e20400604930a1f54a8617486963789
Richie <richieganney@gmail.com>
2019-07-08 15:54:12 +0100

    sinatra: keeping views clean step

--- lib/app.rb
+++ lib/app.rb
@@ -1,8 +1,6 @@
 require 'sinatra'
 require 'shotgun'
 
-set    :session_secret, "here be dragons"
-
 get '/' do
   "Hello World"
 end
@@ -11,22 +9,7 @@ get '/secret' do
   "do one"
 end
 
-get '/your_mum' do
-  "has a weight problem"
-end
-
-get '/your_dad' do
-  "has a drinking solution"
-end
-
-get '/your_sister' do
-  "has a meth career"
-end
-
-get '/your_brother' do
-  "thinks moderation is the key"
-end
-
 get '/cat' do
+  @name = ["Richie", "Tim"].sample
   erb(:index)
 end
