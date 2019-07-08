fc8817a3789f93fb324065fa586012a56e73b239
timdavidcole <timdavidcole@gmail.com>
2019-07-08 15:21:09 +0100

    added kitten image with purple dashed lines

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 ruby '2.6.3'
 
 gem 'sinatra'
+gem 'shotgun'
--- lib/app.rb
+++ lib/app.rb
@@ -1,1 +1,34 @@
 require 'sinatra'
+require 'shotgun'
+
+set    :session_secret, "here be dragons"
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "do one"
+end
+
+get '/your_mum' do
+  "has a weight problem"
+end
+
+get '/your_dad' do
+  "has a drinking solution"
+end
+
+get '/your_sister' do
+  "has a meth career"
+end
+
+get '/your_brother' do
+  "thinks moderation is the key"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed purple'>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
